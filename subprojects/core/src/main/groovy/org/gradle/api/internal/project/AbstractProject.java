aa8a223e0a603989e51194897346790904f11c7e
Lóránt Pintér <lorant@gradle.com>
2015-09-15 11:49:03 +0200

    Remove unused method

--- subprojects/core/src/main/groovy/org/gradle/api/internal/project/AbstractProject.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/project/AbstractProject.java
@@ -967,7 +967,6 @@ public abstract class AbstractProject extends AbstractPluginAware implements Pro
     // Not part of the public API
     public void model(Closure<?> modelRules) {
         ModelRegistry modelRegistry = getModelRegistry();
-        ModelSchemaStore modelSchemaStore = getModelSchemaStore();
         NodeInitializerRegistry nodeInitializerRegistry = getNodeInitializerRegistry();
         if (TransformedModelDslBacking.isTransformedBlock(modelRules)) {
             ClosureBackedAction.execute(new TransformedModelDslBacking(modelRegistry, nodeInitializerRegistry, this.getRootProject().getFileResolver()), modelRules);
